44380189bc179d339fefd035c83a359cb5ca034d
koendebruijn <bruijnk@gmail.com>
2020-09-15 20:38:40 +0200

    Created header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="wrapper">
+    <Header />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Header from '@/components/Header.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    Header,
+  },
 });
 </script>
 
 <style lang="scss">
-#app {
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  color: white;
+  font-family: 'Poppins', sans-serif;
+}
+
+.wrapper {
+  width: 90%;
+  margin: 0 auto;
+  padding: 2em 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1>Welcome</h1>
+    <h4>Add things you want to do today</h4>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+};
+</script>
+
+<style></style>
